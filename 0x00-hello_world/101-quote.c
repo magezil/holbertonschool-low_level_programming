5d0703e52de7d270cd3f763d5ab2123f3bd31bfd
me <28716052+magezil@users.no-reply.github.com>
2017-09-13 22:49:21 -0700

    add document comment

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - prints error message
+ *
+ * Return - 1
+ */
+
 int main(void)
 {
 	char* err = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
