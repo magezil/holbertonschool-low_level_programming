fdd8f94b9b2c86b48f451051a6e0b10e6f2dca12
Liz Magalindan <256@holbertonschool.com>
2017-09-19 17:46:53 +0000

    add function definition

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number to be operated on
+ */
+int print_last_digit(int n)
+{
+	int r = n % 10;
+
+	if (n < 0)
+		r *= -1;
+	_putchar('0' + r);
+	return (r);
+}
