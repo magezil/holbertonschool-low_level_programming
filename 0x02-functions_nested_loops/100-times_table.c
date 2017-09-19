91aecaa9aa8dcfdcc2c7a8b99435ab4fe93672a0
Liz Magalindan <256@holbertonschool.com>
2017-09-19 19:27:01 +0000

    update holberton.h for new function definition

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - prints times table from 0 to n
+ * @n: number of times table
+ *
+ * Description: prints time table from 0 to n as long as n is between 0-15
+ */
+void print_times_table(int n)
+{
+	int i;
+	int m;
+	int j;
+	int temp;
+
+	if (n < 0 || n >= 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(' ');
+				m = i * j;
+				/* Calculate first digit */
+				temp = m / 100;
+				if (temp == 0)
+					_putchar(' ');
+				else
+					_putchar('0' + temp);
+				/* Calculate second digit */
+				temp = (m / 10) % 10;
+				if (temp == 0)
+					_putchar(' ');
+				else
+					_putchar('0' + temp);
+				/* Last digit */
+				_putchar('0' + (m % 10));
+			}
+			if (j < n)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
