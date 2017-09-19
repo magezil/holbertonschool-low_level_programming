3e8deaa51c9a99e5438c8a900594c777406f966d
Liz Magalindan <256@holbertonschool.com>
2017-09-19 19:02:20 +0000

    function definition for add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two numbers and returns the sum
+ * @n: first number to add
+ * @m: second number to add
+ *
+ * Return: sum of n + m
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
