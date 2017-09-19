4074419667b6de8b539bd35eb8008296f1a52692
Liz Magalindan <256@holbertonschool.com>
2017-09-19 19:46:17 +0000

    program that computes sum of multiples less than 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print sum of natural numbers that are multiples of 3 and 5 under 1024
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int end;
+	int sum;
+
+	end = 1024;
+	sum = 0;
+	for (i = 1; i < end; i++)
+	{
+		if (i % 3 == 0)
+			sum += i;
+		else if (i % 5 == 0)
+			sum += i;
+	}
+	printf("The sum of all natural numbers less than %d is %d\n", end, sum);
+	return (0);
+}
