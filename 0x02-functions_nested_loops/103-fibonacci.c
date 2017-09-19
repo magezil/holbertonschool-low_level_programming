15e9297905f77901fad6a559d579c09a4aa62d03
Liz Magalindan <256@holbertonschool.com>
2017-09-19 21:14:58 +0000

    prints all fib numbers less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the Fibonacci numbers less than 4,000,000
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned long int number;
+	unsigned long int first;
+	unsigned long int second;
+	unsigned long int sum;
+
+	number = 4000000;
+	first = 1;
+	second = 2;
+	sum = first;
+	while (sum < number)
+	{
+		if (sum == 1)
+		{
+			printf("%lu, ", first);
+			sum = second;
+		}
+		else if (sum == 2)
+		{
+			printf("%lu, ", second);
+			sum = first + second;
+		}
+		else
+		{
+			printf("%lu", sum);
+			sum = first + second;
+			first = second;
+			second = sum;
+			if (sum < number)
+				printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
