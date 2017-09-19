95b3d9df9a4b58e567a449798c75e3c406912182
Liz Magalindan <256@holbertonschool.com>
2017-09-19 23:37:45 +0000

    refactored code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,53 +9,43 @@
  */
 int main(void)
 {
-	int number, count;
-	unsigned long int first, first2, second, second2, sum, sum2, large;
-	int overflow = 1;
+	int count, overflow = 1;
+	unsigned long int first = 1, first2, second = 2, second2, sum, sum2;
+	unsigned long int large = 10000000000000000;
 
-	number = 98;
-	first = 1;
-	second = 2;
-	sum = first + second;
-	large = 10000000000000000;
-	for (count = 0; count < number; count++)
+	printf("%lu, %lu, ", first, second);
+	for (count = 2; count < 98; count++)
 	{
-		if (count == 0)
-			printf("%lu, ", first);
-		else if (count == 1)
-			printf("%lu, ", second);
-		else if (first < (ULONG_MAX - second) && overflow)
+		if (first < (ULONG_MAX - second) && overflow)
 		{
 			sum = first + second;
 			first = second;
 			second = sum;
 			printf("%lu, ", sum);
+			continue;
 		}
-		else
+		if (overflow)
 		{
-			if (overflow)
-			{
-				first2 = first / large;
-				first %= large;
-				second2 = second / large;
-				second %= large;
-				overflow = !overflow;
-			}
-			sum = first + second;
-			sum2 = first2 + second2;
-			if (sum / large > 1)
-			{
-				sum2 += (sum / large);
-				sum %= large;
-			}
-			printf("%lu%016lu", sum2, sum);
-			if (count < number - 1)
-				printf(", ");
-			first = second;
-			first2 = second2;
-			second = sum;
-			second2 = sum2;
+			first2 = first / large;
+			first %= large;
+			second2 = second / large;
+			second %= large;
+			overflow = !overflow;
 		}
+		sum = first + second;
+		sum2 = first2 + second2;
+		if (sum / large > 1)
+		{
+			sum2 += (sum / large);
+			sum %= large;
+		}
+		printf("%lu%016lu", sum2, sum);
+		if (count < 97)
+			printf(", ");
+		first = second;
+		first2 = second2;
+		second = sum;
+		second2 = sum2;
 	}
 	printf("\n");
 	return (0);
