01ea8f111827a302fcdc10a21145cd5fba625ea2
magezil <256@holbertonschool.com>
2017-09-21 18:48:22 +0000

    correct output and parameters

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -1,18 +1,22 @@
 #include "holberton.h"
 
 /**
- * print_square: prints a # square in terminal
+ * print_square - prints a # square in terminal
  * @n: length of square's side
  */
-void print_square(int size)
+void print_square(int n)
 {
 	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
+		{
 			for (j = 0; j < n; j++)
 				_putchar('#');
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
