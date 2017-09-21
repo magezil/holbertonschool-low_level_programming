858223015980a852b4469b74f781aac5b92a141c
magezil <256@holbertonschool.com>
2017-09-21 18:29:44 +0000

    fix output

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,13 @@
 /**
  * print_most_numbers: prints digits 0 to 9, except 2 and 4, and a new line
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
-	for (int i = '0'; i < '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 2 && i != 4)
+		if (!(i == '2' || i == '4'))
 			_putchar(i);
 	}
 	_putchar('\n');
