f6dd9af5b13730fe687dbf612dd7319cef19c83c
magezil <256@holbertonschool.com>
2017-09-21 20:47:28 +0000

    v 2.0

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,16 @@ int main(void)
 {
 	int i;
 
-
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
-		if (i % 5 == 0)
-			printf("Buzz");
-		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d", i);
-		printf(" ");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
