91bb069b7b7ea300117487a4d0cb4eca6bac0cc1
magezil <256@holbertonschool.com>
2017-09-21 18:59:14 +0000

    correct output

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -1,21 +1,25 @@
 #include "holberton.h"
 
 /**
- * print_triangle: prints a # triangle in terminal
+ * print_triangle - prints a # triangle in terminal
  * @n: length of triangle's side
  */
 void print_triangle(int n)
 {
 	int i, j;
 
-	if (n> 0)
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
+		{
 			for (j = 0; j < n; j++)
-				if (j < i)
+				if (j < (n - i - 1))
 					_putchar(' ');
 				else
 					_putchar('#');
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
