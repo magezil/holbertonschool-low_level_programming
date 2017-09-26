8b56f12cd08f08aa3e3aa48acf49f666567db4de
magezil <256@holbertonschool.com>
2017-09-26 21:06:23 +0000

    update header file and function definition

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert a string to a number
+ * @s: string to convert
+ *
+ * Return: number or 0 if string cannot be converted
+ */
+int _atoi(char *s)
+{
+	int found = 0;
+	unsigned int num = 0;
+	int n = 1;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			n *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			if (!found)
+				found = 1;
+			num = num * 10 + ( *s - '0');
+		}
+		else if (found)
+			break;
+		s++;
+	}
+	return (num * n);
+}
