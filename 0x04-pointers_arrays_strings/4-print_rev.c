72894135ee5b3d9bd7c2663462b4effeff25da84
magezil <256@holbertonschool.com>
2017-09-26 18:16:55 +0000

    add function definition for print_rev

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rev_string - prints string in reverse
+ * @s: string to reverse
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	for (count = _strlen(s) - 1; count >= 0; count--)
+		_putchar(*(s+count));
+	_putchar('\n');
+}
+
+/**
+ * _strlen - return length of a string
+ * @s: string to find the length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+        int count;
+
+        for (count = 0; *s != '\0'; s++)
+                count++;
+
+        return (count);
+}
