f326bb2cbd473da069b129df59f5cae43c59432d
magezil <256@holbertonschool.com>
2017-09-26 18:49:09 +0000

    add function definition

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print every other character of a string starting with the first
+ * @str: the string to print
+ */
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = _strlen(str);
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(*(str+i));
+	}
+	_putchar('\n');
+}
+
+#include "holberton.h"
+
+/**
+ * _strlen - return length of a string
+ * @s: string to find the length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		count++;
+
+	return (count);
+}
