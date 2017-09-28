cbaafe68eaa8722cad5cbe8022e9e5080400e06e
magezil <256@holbertonschool.com>
2017-09-28 21:52:44 +0000

    correct output and fix style

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -14,11 +14,11 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	 	for (j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			if (s[i] != ' ' && (s[i] == encode[j] || (s[i] - upper) == encode[j]))
+			if (s[i] != ' ' && (s[i] == encode[j] || (s[i] == encode[j] - upper)))
 				s[i] = j + '0';
 		}
 	}
-	return s;
+	return (s);
 }
