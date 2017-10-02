ea5759f743f83c8eecb6e4e080d462b4e6ba2499
magezil <256@holbertonschool.com>
2017-10-02 18:16:31 +0000

    add README.md and header files, new function definition

---
+++ 0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: memory to fill
+ * @b: byte used to fill memory
+ * @n: number of bytes of memory to fill
+ *
+ * Return: pointer to filled memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	if (n <= 0)
+		return 0;
+
+	*s = b;
+	return (_memset(++s, b, --n));
+}
