8e96bb37795c376c00d689093fb4144c7deb88ec
magezil <256@holbertonschool.com>
2017-10-02 19:43:31 +0000

    define new function, update README and .h files

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locate character in string
+ * @s: string to search through
+ * @c: character to find
+ *
+ * Return: first occurance of character or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 #0x06-pointers_arrays_strings
 
 0-memset.c - fill memory with a constant byte\
-1-memcpy.c - copy memory area
+1-memcpy.c - copy memory area\
+2-strchr.c - locate character in string
