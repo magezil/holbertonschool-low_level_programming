7ef1812025c25227fa1a770127b7f7d56dd409cc
me <28716052+magezil@users.no-reply.github.com>
2017-10-02 22:30:27 -0700

    use NULL correctly

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strchr - locate character in string
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,8 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#define NULL 0
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
