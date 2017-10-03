1ed7a01d5329fb70ff3164f6f41bc8a0a4a2c860
me <28716052+magezil@users.no-reply.github.com>
2017-10-02 22:35:31 -0700

    use correct NULL

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strpbrk - locate any of a set of bytes in string
--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strstr - find a needle in a haystack, aka locate substring
