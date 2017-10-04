c5c61e2779a4631e5c476f089ab6a878b587900d
magezil <256@holbertonschool.com>
2017-10-04 19:41:01 +0000

    add function definition

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+int _sqrt_recursion_helper(int n, int d);
+
+/**
+ * _sqrt_recursion - compute the natural square root of a number
+ * @n: number to find the square root of
+ *
+ * Return: square root of number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt_recursion_helper(n, n - 1));
+}
+
+/**
+ * _sqrt_recursion_helper - recursive helper function to calculate square root
+ * @n: number to calculate square root of
+ * @d: number to try
+ *
+ * Return: square root of number
+ */
+int _sqrt_recursion_helper(int n, int d)
+{
+	if (d == 0)
+		return (-1);
+	if (n % d == 0 && n / d == d)
+		return (d);
+	return (_sqrt_recursion_helper(n, d - 1));
+}
