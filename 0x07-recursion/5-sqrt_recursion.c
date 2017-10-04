47a38bd813b58f843f315ddd5a724f87cd738bf3
magezil <256@holbertonschool.com>
2017-10-04 21:42:25 +0000

    correct for edge case of sqrt(MAX_INT)

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt_recursion_helper(n, n - 1));
+	return (_sqrt_recursion_helper(n, 2));
 }
 
 /**
@@ -26,9 +26,9 @@ int _sqrt_recursion(int n)
  */
 int _sqrt_recursion_helper(int n, int d)
 {
-	if (d == 1)
+	if (d * d > n || d * d < 0)
 		return (-1);
-	if (n % d == 0 && n / d == d)
+	if (d * d == n)
 		return (d);
-	return (_sqrt_recursion_helper(n, d - 1));
+	return (_sqrt_recursion_helper(n, d + 1));
 }
