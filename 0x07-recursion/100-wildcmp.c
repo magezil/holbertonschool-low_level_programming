8fe6461438dc7fa5f3c0b5d2e4db9b32c7ed3017
magezil <256@holbertonschool.com>
2017-10-05 04:10:30 +0000

    fixed style

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -19,13 +19,14 @@ int wildcmp(char *s1, char *s2)
 	}
 	/* If there are characters in s1 but not s2, they are not the same */
 	if (*s2 == '\0')
-		return 0;
+		return (0);
 
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1 + 1, s2 + 1) ||
+			wildcmp(s1, s2 + 1));
 
 	return (0);
 }
