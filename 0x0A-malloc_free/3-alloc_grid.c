bc9742b269c8873da08e416a6aed242e12ba469b
magezil <256@holbertonschool.com>
2017-10-10 21:23:01 +0000

    new function definition

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - return pointer to a 2D array of integers
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: pointer to 2D array (success) or NULL (failure)
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * height);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
