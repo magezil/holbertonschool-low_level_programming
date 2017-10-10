2f7094a2351574c0ee8877e23220a1171f45cc1f
magezil <256@holbertonschool.com>
2017-10-10 17:31:44 +0000

    add function definition

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - create an array of chars and initialize with specific char
+ * @size: size of array
+ * @c: character to initialize array with
+ *
+ * Return: pointer to new array (success) or NULL (failure)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+		return (ptr);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
+}
