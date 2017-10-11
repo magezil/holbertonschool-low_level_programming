1826d20ff15125220990bcc0155ebe3947643e02
magezil <256@holbertonschool.com>
2017-10-11 21:22:53 +0000

    refactor code

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -11,31 +11,24 @@
 char **strtow(char *str)
 {
 	char **words;
-	int wc = 0, cc = 0, i, j, count = 0, wordon = 0;
+	int wc, cc;
+	int i, j, count;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (wordon == 0 && str[i] != ' ')
-		{
-			wordon = 1;
-			wc++;
-		}
-		if (str[i] == ' ' && wordon == 1)
-			wordon = 0;
-	}
+
+	wc = word_count(str);
 	if (wc == 0)
 		return (NULL);
 	words = malloc(sizeof(char *) * (wc + 1));
 	if (words == NULL)
 		return (NULL);
+
 	for (count = 0, i = 0; i < wc; i++)
 	{
 		while (str[count] == ' ' && str[count] != '\0')
 			count++;
-		for (cc = 0, j = 0; str[count + j] != '\0' && str[count + j] != ' '; j++)
-			cc++;
+		cc = char_count(str, count);
 		words[i] = malloc(sizeof(char) * (cc + 1));
 		if (words[i] == NULL)
 		{
@@ -50,4 +43,46 @@ char **strtow(char *str)
 	}
 	words[i] = NULL;
 	return (words);
+}
+
+/**
+ * word_count - returns number of words in given string delimited by spaces
+ * @str: string to count words
+ *
+ * Return: number of words
+ */
+int word_count(char *str)
+{
+	int i;
+	int wc = 0;
+	int wordon = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (wordon == 0 && str[i] != ' ')
+		{
+			wordon = 1;
+			wc++;
+		}
+		if (str[i] == ' ' && wordon == 1)
+			wordon = 0;
+	}
+	return (wc);
+}
+
+/**
+ * char_count - returns number of chars in given word
+ * @str: string to count chars
+ * @index: index of string for word to count
+ *
+ * Return: number of chars
+ */
+int char_count(char *str, int index)
+{
+	int j;
+	int cc = 0;
+
+	for (j = 0; str[index + j] != '\0' && str[index + j] != ' '; j++)
+		cc++;
+	return (cc);
 }
