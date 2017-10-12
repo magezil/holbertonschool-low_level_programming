3a823c4c48111bca300ce6a0c778a1eb4b43674a
magezil <256@holbertonschool.com>
2017-10-12 19:57:07 +0000

    update function

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates given amount of memory using malloc
+ * @b: amount of memory to allocate
+ *
+ * Return: pointer to allocated memory (success) or NULL (fail)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
