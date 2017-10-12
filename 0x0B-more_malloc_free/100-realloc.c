cf6ca482d14476db5cbb604bdb6bf04bac522bae
magezil <256@holbertonschool.com>
2017-10-12 22:02:59 +0000

    new function, update header and README

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to old memory location allocated by malloc
+ * @old_size: size of old memory location
+ * @new_size: new size of memory to be allocated
+ *
+ * Return: pointer to reallocated memory (success) or NULL (fail)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *newp;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		newp = malloc(new_size);
+		/* return newp - if NULL, will return NULL; else return newp */
+		return (newp);
+	}
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		if (ptr != NULL)
+			free(ptr);
+		return (NULL);
+	}
+
+	newp = malloc(new_size);
+	if (newp == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		newp[i] = *((char *)ptr + i);
+	return (newp);
+}
--- 0x0B-more_malloc_free/README.md
+++ 0x0B-more_malloc_free/README.md
@@ -6,3 +6,4 @@
 1-string_nconcat.c - concatenate a string to n bytes of a second string  
 2-calloc.c - allocate and initialize memory for an array   
 3-array_range.c - create array of integers    
+100-realloc.c - reallocates a memory block with malloc and free  
