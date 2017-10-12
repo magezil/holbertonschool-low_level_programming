37c9701526a7eeba022aaab1ee03bc50df143a59
magezil <256@holbertonschool.com>
2017-10-12 20:48:33 +0000

    new function definition

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -2,5 +2,27 @@
 #include "holberton.h"
 
 /**
+ * array_range - create array of integers
+ * @min: minimum value to include
+ * @max: maximum value to include
  *
+ * Return: pointer to new array (success) or NULL (fail)
  */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int size;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	arr = malloc(sizeof(*arr) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; min + i <= max; i++)
+		arr[i] = min + i;
+	return (arr);
+}
