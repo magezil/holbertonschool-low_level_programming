4d04f78e84bfcb92165e9153b0b410ee9ee74ba8
magezil <256@holbertonschool.com>
2017-10-16 23:25:13 +0000

    changed null checks to print instead of return NULL

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,8 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
+	if (name == NULL)
+		name = "(nil)";
+	if (owner == NULL)
+		owner = "(nil)";
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
