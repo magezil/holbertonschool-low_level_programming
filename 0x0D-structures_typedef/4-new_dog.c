d16ef745c00662e4d25800c119b9fb3e7e177f68
magezil <256@holbertonschool.com>
2017-10-16 23:13:13 +0000

    function definition and header update

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -12,13 +12,61 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
+	char *nname;
+	char *nowner;
 
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = name;
+	nname = malloc(_strlen(name) * sizeof(char));
+	if (nname == NULL)
+		return (NULL);
+	nowner = malloc(_strlen(owner) * sizeof(char));
+	if (nowner == NULL)
+		return (NULL);
+	_strcpy(nname, name);
+	_strcpy(nowner, owner);
+	ndog->name = nname;
 	ndog->age = age;
-	ndog->owner = owner;
+	ndog->owner = nowner;
 
 	return (ndog);
+}
+
+/**
+ * _strcpy - copies given string
+ * @dest: pointer that is the destination of the copy
+ * @src: string to copy
+ *
+ * Return: pointer to copy of string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *temp;
+
+	temp = dest;
+	while (*src != '\0')
+	{
+		*temp = *src;
+		temp++;
+		src++;
+	}
+	*temp = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - return length of a string
+ * @s: string to find the length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		count++;
+
+	return (count);
 }
