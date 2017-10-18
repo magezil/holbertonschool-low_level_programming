604348855eddf6b8eede90bfc2a9e659b8d3e160
magezil <256@holbertonschool.com>
2017-10-18 17:45:15 +0000

    new function definition

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search through
+ * @size: size of array
+ * @cmp: function to use to compare
+ *
+ * Return: index of first element in which cmp does not return (0)
+ *         or (-1) if there are no such elements
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+	return (-1);
+}
