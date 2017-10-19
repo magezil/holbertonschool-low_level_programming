0f2f1feaf9bc31230c4b6b007186932360f798a2
magezil <256@holbertonschool.com>
2017-10-19 02:02:37 +0000

    new function and updated README

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print opcodes of main function
+ * @ac: count of command line arguments
+ * @av: array of command line arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int ac, char *av[])
+{
+	int num_bytes, i;
+	char *opcode;
+	
+	/* Check if number of arguments is incorrect */
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	
+	num_bytes = atoi(av[1]);
+	/* Check if number of bytes is negative */
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	
+	/* convert pointer to function to take advantage of address */
+	opcode = (char *)&main;
+	for (i = 0; i < num_bytes; i++)
+	{
+		/* &0xff to mask any leading ffs */
+		printf("%02x ", opcode[i] & 0xff);
+	}
+	printf("\n");
+	return (0);
+}
--- 0x0E-function_pointers/README.md
+++ 0x0E-function_pointers/README.md
@@ -5,10 +5,10 @@
 0-print_name.c - prints a name  
 1-array_iterator.c - executes given function on each element of given array  
 2-int_index.c - search for integer  
+100-main_opcodes.c - print opcodes for main function
 
 ### Files for program to perform simple operations
-3-calc.h - header file for program to performs simple operations
-3-op_functions.c - file with operation functions
-3-get_op_func.c - file for function to determine which operation to perform
+3-calc.h - header file for program to performs simple operations  
+3-op_functions.c - file with operation functions  
+3-get_op_func.c - file for function to determine which operation to perform  
 3-main.c - file with ain function  
-
