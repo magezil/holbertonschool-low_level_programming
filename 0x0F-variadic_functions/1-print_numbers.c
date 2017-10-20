103ba525b3cfd6eba6375e353e876a6a08dd75fc
magezil <256@holbertonschool.com>
2017-10-20 18:17:29 +0000

    new function definition

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print given numbers followed by a new line
+ * @separator: string to print between numbers
+ * @n: number of integers passed to function
+ * @...: integers to print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
