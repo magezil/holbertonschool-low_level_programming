962fad343085616658226cc7c74bf07d4f33e465
magezil <256@holbertonschool.com>
2017-10-21 20:39:21 +0000

    v2.0

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include "variadic_functions.h"
+#include "variadic_functions2.h"
 
 /**
  * print_all - print anything
@@ -9,40 +7,82 @@
  */
 void print_all(const char * const format, ...)
 {
+	print_t prints[] = {
+		{"c", print_char},
+		{"i", print_number},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 	va_list args;
-	int i;
-	char *message;
+	int i, j;
 	char *sep;
 
 	va_start(args, format);
-	i = 0;
 	sep = "";
+	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
-		switch (format[i])
+		j = 0;
+		while (prints[j].type != NULL)
 		{
-		case 'c':
-			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-			break;
-		case 'i':
-			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-			break;
-		case 'f':
-			printf("%s%f", sep, va_arg(args, double));
-			sep = ", ";
-			break;
-		case 's':
-			message = va_arg(args, char *);
-			if (message == NULL)
-				message = "(nil)";
-			printf("%s%s", sep, message);
-			sep = ", ";
-			break;
+			/* only compare current format with prints type once */
+			if (format[i] == *(prints[j].type))
+			{
+				prints[j].f(sep, args);
+				sep = ", ";
+				break;
+			}
+			j++;
 		}
 		i++;
 	}
 	printf("\n");
 	va_end(args);
+}
+
+/**
+ * print_string - print strings
+ * @separator: string to print after string
+ * @args: list to retrive string from
+ */
+void print_string(const char *separator, va_list args)
+{
+	char *message;
+
+	message = va_arg(args, char *);
+	if (message == NULL)
+		message = "(nil)";
+	printf("%s%s", separator, message);
+}
+
+/**
+ * print_number - print given numbers
+ * @separator: string to print after number
+ * @args: list to retrieve int from
+ */
+void print_number(const char *separator, va_list args)
+{
+	printf("%s%d", separator, va_arg(args, int));
+}
+
+/**
+ * print_char - print a character
+ * @separator: string to print after character
+ * @args: list to retrieve char from
+ */
+void print_char(const char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
+}
+
+
+/**
+ * print_float - print float
+ * @separator: string to print after float
+ * @args: list to retrieve float from
+ */
+void print_float(const char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
 }
