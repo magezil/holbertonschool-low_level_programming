315aae288268648a5905805e195a06a2675bb89b
magezil <256@holbertonschool.com>
2017-10-21 20:51:01 +0000

    revert to working version

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,4 +1,6 @@
-#include "variadic_functions2.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * print_all - print anything
@@ -7,82 +9,40 @@
  */
 void print_all(const char * const format, ...)
 {
-	print_t prints[] = {
-		{"c", print_char},
-		{"i", print_number},
-		{"f", print_float},
-		{"s", print_string},
-		{NULL, NULL}
-	};
 	va_list args;
-	int i, j;
+	int i;
+	char *message;
 	char *sep;
 
 	va_start(args, format);
-	sep = "";
 	i = 0;
+	sep = "";
 	while (format != NULL && format[i] != '\0')
 	{
-		j = 0;
-		while (prints[j].type != NULL)
+		switch (format[i])
 		{
-			/* only compare current format with prints type once */
-			if (format[i] == *(prints[j].type))
-			{
-				prints[j].f(sep, args);
-				sep = ", ";
-				break;
-			}
-			j++;
+		case 'c':
+			printf("%s%c", sep, va_arg(args, int));
+			sep = ", ";
+			break;
+		case 'i':
+			printf("%s%d", sep, va_arg(args, int));
+			sep = ", ";
+			break;
+		case 'f':
+			printf("%s%f", sep, va_arg(args, double));
+			sep = ", ";
+			break;
+		case 's':
+			message = va_arg(args, char *);
+			if (message == NULL)
+				message = "(nil)";
+			printf("%s%s", sep, message);
+			sep = ", ";
+			break;
 		}
 		i++;
 	}
 	printf("\n");
 	va_end(args);
-}
-
-/**
- * print_string - print strings
- * @separator: string to print after string
- * @args: list to retrive string from
- */
-void print_string(const char *separator, va_list args)
-{
-	char *message;
-
-	message = va_arg(args, char *);
-	if (message == NULL)
-		message = "(nil)";
-	printf("%s%s", separator, message);
-}
-
-/**
- * print_number - print given numbers
- * @separator: string to print after number
- * @args: list to retrieve int from
- */
-void print_number(const char *separator, va_list args)
-{
-	printf("%s%d", separator, va_arg(args, int));
-}
-
-/**
- * print_char - print a character
- * @separator: string to print after character
- * @args: list to retrieve char from
- */
-void print_char(const char *separator, va_list args)
-{
-	printf("%s%c", separator, va_arg(args, int));
-}
-
-
-/**
- * print_float - print float
- * @separator: string to print after float
- * @args: list to retrieve float from
- */
-void print_float(const char *separator, va_list args)
-{
-	printf("%s%f", separator, va_arg(args, double));
 }
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,27 +1,9 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
-#include <stdarg.h>
-#include <stdio.h>
-
-/**
- * struct print_functions - Struct print_functions
- *
- * @type: The format to print
- * @f: The print function to use
- */
-typedef struct print_functions
-{
-	char *type;
-	void (*f)();
-} print_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_number(const char *separator, va_list list);
-void print_string(const char *separator, va_list list);
-void print_char(const char *separator, va_list list);
-void print_float(const char *separator, va_list list);
 
 #endif
