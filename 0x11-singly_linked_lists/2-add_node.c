70c1b641eb409165a30a7d712510c01a6536da3d
magezil <256@holbertonschool.com>
2017-10-26 22:15:08 +0000

    add NULL check

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -13,6 +13,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	char *cpy;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
