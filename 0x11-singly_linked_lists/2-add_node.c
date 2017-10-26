d74a44ad41991daa877cb1191cd3ca6fbac0d9dd
magezil <256@holbertonschool.com>
2017-10-26 22:11:53 +0000

    add new function

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the beginning of a linked list_t list
+ * @head: pointer to pointer to list
+ * @str: string to include in new node
+ *
+ * Return: address of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *cpy;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	cpy = strdup(str);
+	new->str = cpy;
+	new->len = _strlen(cpy);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * _strlen - return length of a string
+ * @s: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		count++;
+
+	return (count);
+}
