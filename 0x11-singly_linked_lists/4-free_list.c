b47a5e192f547fd063de9e1584d10974d3b4424e
magezil <256@holbertonschool.com>
2017-10-27 00:20:15 +0000

    refactor temp assignment

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -7,15 +7,14 @@
 void free_list(list_t *head)
 {
 	list_t *temp;
+
 	if (head == NULL)
 		return;
-	temp = head->next;
 	while (head != NULL)
 	{
+		temp = head->next;
 		free(head->str);
 		free(head);
 		head = temp;
-		if (temp != NULL)
-			temp = temp->next;
 	}
 }
