a0dca44126edd6e6f9db3e3402885b538f5618b8
magezil <256@holbertonschool.com>
2017-10-30 17:50:26 +0000

    new function definition

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all elements of a listint_t list
+ * @h: pointer to list to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
