2d449ec76906b40eadc657a9ced43d0884b72d10
magezil <256@holbertonschool.com>
2017-10-30 20:50:35 +0000

    add new function definition

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - function adds a new node at the end of a *listint_t* list
+ * @head: pointer to pointer to list
+ * @n: n for new node
+ *
+ * Return: address to new element or NULL if failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	temp = *head;
+	if (temp == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
+}
--- 0x12-more_singly_linked_lists/README.md
+++ 0x12-more_singly_linked_lists/README.md
@@ -15,3 +15,22 @@
 * function adds a new node to the beginning of a *listint_t* list
 * Prototype: listint_t *add_nodeint(listint_t **head, const int n);
 * Return: the address of the new element or NULL if failed
+
+### 3-add_nodeint_end.c
+* function adds a new node at the end of a *listint_t* list
+* Prototype: listint_t *add_nodeint_end(listint_t **head, const int n);
+* Return: the address of the new element or NULL if failed
+
+### 4-free_listint.c
+
+### 5-free_listint2.c
+
+### 6-pop_listint.c
+
+### 7-get_nodeint.c
+
+### 8-sum_listint.c
+
+### 9-insert_nodeint.c
+
+### 10-delete_nodeint.c
