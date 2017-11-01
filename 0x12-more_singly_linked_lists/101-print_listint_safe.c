73f456cd63770a12044cb1b773f0b2bc7f9e745c
magezil <256@holbertonschool.com>
2017-11-01 01:30:06 +0000

    fix error output

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,7 @@ size_t print_listint_safe(const listint_t *head)
 		return (0);
 	visited = create_array(buff_size);
 	if (visited == NULL)
-		exit(98);
+		return (0);
 
 	count = 0;
 	temp = (listint_t *) head;
