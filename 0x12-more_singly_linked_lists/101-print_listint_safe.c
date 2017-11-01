cee110518dcefc82ceffeaadc95f6e64554dbc24
magezil <256@holbertonschool.com>
2017-11-01 04:12:52 +0000

    v2.0

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,117 +10,28 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	unsigned int count;
-	unsigned int buff_size = 100;
-	listint_t **visited;
-	listint_t *temp;
+	unsigned int i;
+	listint_t *temp1;
+	listint_t *temp2;
 
 	if (head == NULL)
 		return (0);
-	visited = create_array(buff_size);
-	if (visited == NULL)
-		return (0);
-
 	count = 0;
-	temp = (listint_t *) head;
-	while (temp != NULL)
+	temp1 = (listint_t *) head;
+	while (temp1 != NULL)
 	{
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		if (count >= buff_size)
-		{
-			visited = _realloc(visited, buff_size, buff_size + 100);
-			buff_size += 100;
-		}
-		visited[count] = temp;
+		printf("[%p] %d\n", (void *)temp1, temp1->n);
 		count++;
-		temp = temp->next;
-		if (temp != NULL && !checker(temp, visited, buff_size))
+		temp1 = temp1->next;
+		for (i = 0, temp2 = (listint_t *) head; i < count; i++)
 		{
-			printf("-> [%p] %d\n", (void *)temp, temp->n);
-			free(visited);
-			exit(98);
+			if (temp1 == temp2)
+			{
+				printf("-> [%p] %d\n", (void *)temp1, temp1->n);
+				exit(98);
+			}
+			temp2 = temp2->next;
 		}
 	}
-	free(visited);
 	return (count);
-}
-
-/**
- * create_array - create an array of pointers to linked list nodes
- * @size: size of array
- *
- * Return: pointer to new array (success) or NULL (failure)
- */
-listint_t **create_array(unsigned int size)
-{
-	listint_t **ptr;
-	unsigned int i;
-
-	if (size == 0)
-		return (NULL);
-
-	ptr = malloc(sizeof(listint_t *) * size);
-	if (ptr == NULL)
-		return (ptr);
-
-	for (i = 0; i < size; i++)
-		ptr[i] = NULL;
-	return (ptr);
-}
-
-/**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to old memory location allocated by malloc
- * @old: size of old memory location
- * @new: new size of memory to be allocated
- *
- * Return: pointer to reallocated memory (success) or NULL (fail)
- */
-listint_t **_realloc(listint_t **ptr, unsigned int old, unsigned int new)
-{
-	listint_t **newp;
-	unsigned int i;
-
-	if (ptr == NULL)
-	{
-		newp = malloc(sizeof(listint_t) * new);
-		/* return newp - if NULL, will return NULL; else return newp */
-		return (newp);
-	}
-	if (old == new)
-		return (ptr);
-	if (new == 0)
-	{
-		if (ptr != NULL)
-			free(ptr);
-		return (NULL);
-	}
-
-	newp = malloc(new);
-	if (newp == NULL)
-		return (NULL);
-
-	for (i = 0; i < old && i < new; i++)
-		newp[i] = ptr[i];
-	free(ptr);
-	return (newp);
-}
-
-/**
- * checker - function checks if given address is in given list
- * @node: address of current node
- * @list: list of addresses
- * @size: size of list
- *
- * Return: 1 if current is not found, 0 otherwise
- */
-int checker(listint_t *node, listint_t **list, int size)
-{
-	int i;
-
-	for (i = 0; i < size; i++)
-	{
-		if (node == list[i])
-			return (0);
-	}
-	return (1);
 }
