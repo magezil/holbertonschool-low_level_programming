8cfa19f11c991565791759633ecca710e50cf898
magezil <256@holbertonschool.com>
2017-11-02 20:38:10 +0000

    account for getting bit outside byte

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -18,6 +18,8 @@ int get_bit(unsigned long int n, unsigned int index)
 		if (mask > mask << 1)
 			break;
 		mask = mask << 1;
+		if (mask > n && i % 4 != 0)
+			return (-1);
 	}
 	if ((mask & n) > 0)
 		return (1);
