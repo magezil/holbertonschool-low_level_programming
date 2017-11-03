3b19a3863132066019843daa126c8c8890c05e70
magezil <256@holbertonschool.com>
2017-11-03 19:09:23 +0000

    v2.0 based off concepts from PLD

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -9,27 +9,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num;
-	unsigned int two;
-	int index;
 
 	if (b == NULL)
 		return (0);
-	index = 0;
+	num = 0;
 	while (*b != '\0')
 	{
-		if (!(*b == '0' || *b == '1'))
+		num <<= 1;
+		if (*b == '1')
+			num |= 1;
+		else if (*b != '0')
 			return (0);
-		index++;
 		b++;
-	}
-	num = 0;
-	two = 1;
-	while (index > 0)
-	{
-		b--;
-		num += two * (*b - '0');
-		index--;
-		two *= 2;
 	}
 	return (num);
 }
