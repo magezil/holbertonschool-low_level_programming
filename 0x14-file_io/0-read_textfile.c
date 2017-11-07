c8d42b06f846bffa8908482dc95eea5f52fac3b2
magezil <256@holbertonschool.com>
2017-11-07 22:51:17 +0000

    close file

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buff == NULL)
 		return (0);
 	/* open file and check success */
-	fd = open(filename, O_RDONLY, 600);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	/* read file and check success */
@@ -32,6 +32,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	/* print file and check success */
 	printed = write(STDOUT_FILENO, buff, red);
 	if (printed == -1 || printed != red)
+		return (0);
+	/* close and check for success */
+	fd = close(fd);
+	if (fd == -1)
 		return (0);
 	return (printed);
 }
