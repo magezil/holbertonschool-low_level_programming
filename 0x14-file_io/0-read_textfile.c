90bffebe723b41ef40377289216fd9c5f3ef5172
magezil <256@holbertonschool.com>
2017-11-07 22:33:44 +0000

    add checks for read success

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -13,18 +13,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buff;
 
+	/* check invalid filename */
 	if (filename == NULL)
 		return (0);
-
+	/* create buffer and check if valid */
 	buff = malloc(letters);
 	if (buff == NULL)
 		return (0);
-
+	/* open file and check success */
 	fd = open(filename, O_RDONLY, 600);
 	if (fd == -1)
 		return (0);
+	/* read file and check success */
 	red = read(fd, buff, letters);
+	if (red == -1)
+		return (0);
 	buff[red] = '\0';
+	/* print file and check success */
 	printed = write(STDOUT_FILENO, buff, red);
 	if (printed == -1 || printed != red)
 		return (0);
