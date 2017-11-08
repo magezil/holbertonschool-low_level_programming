3e276d6533485a9b8b1eae7bc89c7b1c1d14cb4f
magezil <256@holbertonschool.com>
2017-11-08 22:23:39 +0000

    clean up code

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,101 +14,42 @@
 int main(int ac, char **av)
 {
 	char buff[BUFF_SIZE];
-	int fd0, fd1, num_read, num_write, close_flag0, close_flag1;
+	int fd0, fd1, num_read, num_write;
 	mode_t mode;
 
 	if (ac != 3) /* check number of arguments */
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd0 = open(av[1], O_RDONLY);
-	open_check(fd0, 0, av[1]);
+	if (fd0 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd1 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	open_check(fd0, fd1, av[2]);
-	num_read = BUFF_SIZE;
-	while (num_read == BUFF_SIZE) /* read file until end of file */
-	{
-		num_read = read(fd0, buff, BUFF_SIZE);
-		read_check(fd0, fd1, num_read, av[1]);
-		num_write = write(fd1, buff, num_read);
-		if (num_read != num_write)
-			num_write = -1;
-		write_check(fd0, fd1, num_write, av[2]);
-	}
-	close_flag0 = close(fd0);
-	close_flag1 = close(fd1);
-	close_check(fd0, close_flag0);
-	close_check(fd1, close_flag1);
-	return (0);
-}
-
-/**
- * open_check - check if open file
- * @fd0: file_source
- * @fd1: file_destination
- * @file_s: name of file
- */
-void open_check(int fd0, int fd1, char *file_s)
-{
-	if (fd0 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_s);
-		exit(98);
-	}
 	if (fd1 == -1)
 	{
-		if (fd0 != -1)
+		close(fd0);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
+	do {
+		num_read = read(fd0, buff, BUFF_SIZE);
+		if (num_read == -1)
+		{
 			close(fd0);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_s);
-		exit(99);
-	}
-}
-
-/**
- * write_check - check if file_destination can be opened and written
- * @fd0: file_source
- * @fd1: file_destination
- * @flag: determines whether write was success or not
- * @filename: name of file to write
- */
-void write_check(int fd0, int fd1, int flag, char *filename)
-{
-	if (flag == -1)
-	{
-		close(fd0);
-		close(fd1);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-}
-
-/**
- * read_check - check if file_source can be opened and read
- * @fd0: file_source
- * @fd1: file_destination
- * @flag: determines whether write was success or not
- * @filename: name of file to read
- */
-void read_check(int fd0, int fd1, int flag, char *filename)
-{
-	if (flag == -1)
-	{
-		close(fd0);
-		close(fd1);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-}
-
-/**
- * close_check - check if close file success
- * @fd: first file to close
- * @close_flag: check if close was successful
- */
-void close_check(int fd, int close_flag)
-{
-	if (close_flag == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
-		exit(100);
-	}
+			close(fd1);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		num_write = write(fd1, buff, num_read);
+		if (num_write == -1 || num_read != num_write)
+		{
+			close(fd0);
+			close(fd1);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (num_read == BUFF_SIZE);
+	if (close(fd0) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd0), exit(100);
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1), exit(100);
+	return (0);
 }
