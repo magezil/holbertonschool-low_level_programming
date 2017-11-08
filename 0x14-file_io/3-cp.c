ddea4e60374b527b1a82b9b569783a3f6b4ae675
magezil <256@holbertonschool.com>
2017-11-08 22:31:03 +0000

    clean up code

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int ac, char **av)
 	int fd0, fd1, num_read, num_write;
 	mode_t mode;
 
-	if (ac != 3) /* check number of arguments */
+	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd0 = open(av[1], O_RDONLY);
 	if (fd0 == -1)
@@ -33,22 +33,23 @@ int main(int ac, char **av)
 		num_read = read(fd0, buff, BUFF_SIZE);
 		if (num_read == -1)
 		{
-			close(fd0);
-			close(fd1);
+			close(fd0), close(fd1);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		num_write = write(fd1, buff, num_read);
 		if (num_write == -1 || num_read != num_write)
 		{
-			close(fd0);
-			close(fd1);
+			close(fd0), close(fd1);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	} while (num_read == BUFF_SIZE);
 	if (close(fd0) == -1)
+	{
+		close(fd1);
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd0), exit(100);
+	}
 	if (close(fd1) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1), exit(100);
 	return (0);
