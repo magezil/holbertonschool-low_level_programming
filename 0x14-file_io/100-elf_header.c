ae3b4b5204c9b84d97cd0cdd6f8453fb3c3958e2
magezil <256@holbertonschool.com>
2017-11-12 07:16:06 +0000

    compile with flags

--- 0x14-file_io/100-elf_header.c
+++ 0x14-file_io/100-elf_header.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <elf.h>
 #include "holberton.h"
 
 int cmp(char *s1, char *s2);
@@ -16,7 +16,8 @@ int main(int ac, char **av)
 	int fd, num_read, i;
 	char buff[BUFF_SIZE];
 	char elf[] = {0x7f, 0x45, 0x4c, 0x46, 0x0};
-
+/*	struct Elf32_Ehdr head;
+*/
 	if (ac != 2)
 		dprintf(STDERR_FILENO, "Usage: elf_header elf_file\n"), exit(98);
 	fd = open(av[1], O_RDONLY);
@@ -27,11 +28,15 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	buff[num_read] = '\0';
 	if (cmp(elf, buff) == 0)
-	{
+/*	if (e_indent[0] == 0x7f &&
+		e_indent[1] == 'E' &&
+		e_indent[2] == 'L' &&
+		e_indent[3] == 'F')
+*/	{
 		printf("ELF\n");
 		printf("Magic:\t");
 		for (i = 0; i < 15; i++)
-			printf("%02hhx ", buff[i]);
+			printf("%02x ", buff[i]);
 		printf("\n");
 	}
 	else
