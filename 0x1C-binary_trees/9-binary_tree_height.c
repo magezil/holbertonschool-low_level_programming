a6063b832057249efb90abea8b2048ac914c4207
magezil <256@holbertonschool.com>
2018-03-06 03:36:55 +0000

    Delete unneeded variable

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -10,17 +10,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lsum;
 	size_t rsum;
-	size_t sum;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-	sum = 0;
 	lsum = binary_tree_height(tree->left);
 	rsum = binary_tree_height(tree->right);
 	if (lsum > rsum)
-		sum = lsum + 1;
-	else
-		sum = rsum + 1;
-	
-	return (sum);
+		return (lsum + 1);
+	return (rsum + 1);
 }
