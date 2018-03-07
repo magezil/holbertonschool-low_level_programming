e0c2a7efe29fcecd7eeb4dee7b3eceae1a347923
magezil <256@holbertonschool.com>
2018-03-07 20:30:50 +0000

    Update binary tree balance

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -25,7 +25,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t rsum;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	lsum = binary_tree_height(tree->left);
 	rsum = binary_tree_height(tree->right);
 	if (lsum > rsum)
